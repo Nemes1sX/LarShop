b631a5cff63d41cb1bd016e17a2a3cf52ffe4422
Nemes1sX <kazstonk@gmail.com>
2019-02-09 20:04:23 +0200

    SUkurta

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new \App\Product([
+            
+            'imagePath' => 'https://img.thewhiskyexchange.com/900/rum_cap2.jpg',
+            'title' => 'Captain Morgan',
+            'decsription' => 'Skanus Romas',
+            'price' => '5.5'
+        ]);
+        $product->save(); ////
+    }
+}
