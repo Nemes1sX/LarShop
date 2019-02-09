a95f55113119db939f936547706194b95c60963c
Nemes1sX <kazstonk@gmail.com>
2019-02-09 17:51:03 +0200

    SUkurta

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
 
 <div class="collapse navbar-collapse" id="navbarSupportedContent"  style="background-color: #eeeeee">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <!--<li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
@@ -25,7 +25,7 @@
       </li>
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+      </li>-->
     </ul>
     <form class="form-inline my-2 my-lg-0">
         <a href="#" style="color: grey; padding-right: 5px;"><i class="fas fa-heart"></i>Wishlist</a> 
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,4 +1,39 @@
 @extends('layouts.layout')
 @section('content')
-<h3>Shopping cart</h3>
+<div class="row">
+    <div class="col-sm-4 col-md-6 col-lg-4">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-thumbanil" src="https://img.thewhiskyexchange.com/900/rum_cap2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="clearfix">
+                    <div class="pull-center"><h6>5e</h6></div> 
+                    <a href="#" class="btn btn-primary pull-right">Add To Wishlist</a>
+                    <a href="#" class="btn btn-success pull-right">Add To Cart</a>
+                </div>
+        </div>
+    </div>
+   </div> 
+    <div class="col-sm-4 col-md-6 col-lg-4">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4 col-md-6 col-lg-4">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>            
 @endsection
