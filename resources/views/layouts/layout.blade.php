2159d651ce3cb091a9d7c95421fadb3d4f0cae30
Nemes1sX <kazstonk@gmail.com>
2019-02-10 15:03:41 +0200

    Pakeitimai

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,6 +8,11 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/solid.css" integrity="sha384-r/k8YTFqmlOaqRkZuSiE9trsrDXkh07mRaoGBMoDcmA58OHILZPsk29i2BsFng1B" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/brands.css" integrity="sha384-BKw0P+CQz9xmby+uplDwp82Py8x1xtYPK3ORn/ZSoe6Dk3ETP59WCDnX+fI1XCKK" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/fontawesome.css" integrity="sha384-4aon80D8rXCGx9ayDt85LbyUHeMWd3UiBaWliBlJ53yzm9hqN21A+o1pqoyK04h+" crossorigin="anonymous">
+  <!-- Latest compiled and minified CSS -->
+
+
+<!-- Latest compiled and minified JavaScript -->
+
 
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
   <link href="{{ asset('css/shopping.css') }}" rel="stylesheet" type="text/css" />
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,52 +1,26 @@
 @extends('layouts.layout')
 @section('content')
-@foreach($products->chunk(3) as $productChunk)
+@foreach($products->chunk(3) as $chunk)
 <div class="row">
-    @foreach($productChunk as $product)
-    <div class="col-sm-4 col-md-6 col-lg-4">
-        <div class="thumbnail" style="width: 18rem;">
-            <img class="card-img-top thumbnail" src="{{$product->imagePath}}" alt="Card image cap">
+    @foreach($chunk as $product)
+    <div class="col-sm-6 col-md-4 col-lg-4">
+        <div class="thumbnail product" style="width: 18rem;">
+            <img class="card-img-top thumbnail img-responsive" src="{{$product->imagePath}}" alt="Card image cap">
             <div class="caption">
                 <h5 class="card-title">{{$product->title}}</h5>
                 <p class="card-text">Kategorija: {{$product->category}}</p>
                 <p class="card-text">{{$product->description}}</p>
                 <div class="clearfix">
-                    <div class="pull-left price">{{$product->price}}</div> 
-                    <a href="#" class="btn btn-primary pull-right">Įdėti į Wishlist</a>
-                    <a href="#" class="btn btn-success pull-right">Pridėti į krepšelį</a>
+                    <div align="center">
+                        <h3 class="price">{{$product->price}}</h3>
+                        <div class="pull-left"><a href="#" class="btn btn-primary wishlist"><i class="fas fa-heart"></i> Įdėti į Wishlist</a></div>
+                        <div class="pull-right"><a href="#" class="btn btn-success  cart"><i class="fas fa-shopping-cart"></i>Pridėti į krepšelį</a></div>
+                    </div>
                 </div>
             </div>
-            @endforeach
-        </div>
-       @endforeach 
-   </div> 
-    <!--<div class="col-sm-4 col-md-6 col-lg-4">
-        <div class="thumbnail" style="width: 18rem;">
-            <img class="card-img-top thumbnail" src="https://img.thewhiskyexchange.com/900/rum_cap2.jpg" alt="Card image cap">
-            <div class="caption">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <div class="clearfix">
-                    <div class="pull-left price">5.00$</div> 
-                    <a href="#" class="btn btn-primary pull-right">Add To Wishlist</a>
-                    <a href="#" class="btn btn-success pull-right">Add To Cart</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-4 col-md-6 col-lg-4">
-        <div class="thumbnail" style="width: 18rem;">
-            <img class="card-img-top thumbnail" src="https://img.thewhiskyexchange.com/900/rum_cap2.jpg" alt="Card image cap">
-            <div class="caption">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <div class="clearfix">
-                    <div class="pull-left price">5.00$</div> 
-                    <a href="#" class="btn btn-primary pull-right">Add To Wishlist</a>
-                    <a href="#" class="btn btn-success pull-right">Add To Cart</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>-->       
-@endsection
+        </div> 
+    </div>    
+    @endforeach  
+</div>    
+@endforeach    
+@endsection  
