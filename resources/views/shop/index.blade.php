778d8baae156ae55d3f7aaf1d7285d8af63bdfd6
Nemes1sX <kazstonk@gmail.com>
2024-07-25 00:51:27 +0300

    Fix route bugs and remove lithuanian text

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -9,42 +9,44 @@
             <!--<label>Filtruoti</label>-->
             <select name="category">
                 <option value="all">Visi</option> 
-                <option value="Degtinė">Degtinė</option>
-                <option value="Viskis">Viskis</option>
+                <option value="Degtinė">Vodka</option>
+                <option value="Viskis">Whiskey</option>
             </select>
            <!-- <label>Rūšiuoti</label>-->
             <select name="ascdesc">
-                <option value="priceasc">Kaina 1-100</option>
-                <option value="pricedesc">Kaina 100-1</option>
-                <option value="nameasc">Pavadinimas A-Z</option>
-                <option value="namedesc">Pavadinimas Z-A</option>
+                <option value="priceasc">Price 1-100</option>
+                <option value="pricedesc">Price 100-1</option>
+                <option value="nameasc">Name A-Z</option>
+                <option value="namedesc">Name Z-A</option>
             </select>
-            <button type="submit" class="btn btn-success">Rūšiuoti</button>    
+            <button type="submit" class="btn btn-success">Sort</button>    
         </form>    
     </div>    
 </div>
-@foreach($products->chunk(3) as $chunk) <!-- Išorinis ciklas kuris, prekės išveda po 3 eilutėje -->
+@forelse($products->chunk(3) as $chunk) 
 <div class="row">
-    @foreach($chunk as $product) <!-- Vidinis ciklas -->
+    @foreach($chunk as $product) 
     <div class="col-sm-6 col-md-4 col-lg-4">
         <div class="thumbnail product" style="width: 18rem;">
             <img class="card-img-top thumbnail img-responsive" src="{{$product->imagePath}}" alt="Card image cap">
             <div class="caption">
                 <h5 class="card-title">{{$product->title}}</h5>
-                <p class="card-text">Kategorija: {{$product->category}}</p>
-                <p class="card-text">Valstybė: {{$product->country}}</p>
+                <p class="card-text">Category: {{$product->category}}</p>
+                <p class="card-text">Country: {{$product->country}}</p>
                 <p class="card-text">{{$product->description}}</p>
                 <div class="clearfix">
                     <div class="center">
                         <h3 class="price">{{$product->price}}€</h3>
-                        <a href="{{ route('product.addToWishlist', $product->id)}}" class="btn btn-primary wishlist"><i class="fas fa-heart"></i> Įdėti į Wishlist</a>
-                        <a href="{{ route('product.addToCart', $product->id)}}" class="btn btn-success  cart"><i class="fas fa-shopping-cart"></i>Pridėti į krepšelį</a>
+                        <a href="{{ route('product.addToWishlist', $product->id)}}" class="btn btn-primary wishlist"><i class="fas fa-heart"></i> Add to wishlist</a>
+                        <a href="{{ route('product.addToCart', $product->id)}}" class="btn btn-success  cart"><i class="fas fa-shopping-cart"></i>Add to cart</a>
                     </div>
                 </div>
             </div>
         </div> 
     </div>    
-    @endforeach  
+    @endforeach
 </div>    
-@endforeach    
+@empty
+    <h6 class="text-center">No products</h6>
+@endforelse       
 @endsection  
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::name('product.*')->controller(\App\Http\Controllers\ProductController::class)->group(function () {
+Route::name('product.')->controller(\App\Http\Controllers\ProductController::class)->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('whiskey', 'whiskey')->name('index');
     Route::get('vodka', 'vodka')->name('index');
@@ -27,10 +27,10 @@ Route::name('product.*')->controller(\App\Http\Controllers\ProductController::cl
     Route::post('sorting', 'ProductController@sorting')->name('shop.index');
     Route::get('cart', 'ProductController@cart')->name('shop.shoppingcart');
     Route::get('wishlist', 'ProductController@wishlist')->name('shop.wishlist');
-    Route::get('/add-to-cart/{id}','ProductController@getAddToCart')->name('product.addToCart');
-    Route::get('/deleteItem/{id}', 'ProductController@deleteItem')->name('product.deleteItem');
-    Route::get('deletecart', 'ProductController@deleteCart')->name('product.deleteCart');
-    Route::get('/add-to-wishlist/{id}','ProductController@getAddToWishlist')->name('product.addToWishlist');
+    Route::get('/add-to-cart/{id}','ProductController@getAddToCart')->name('addToCart');
+    Route::get('/deleteItem/{id}', 'ProductController@deleteItem')->name('deleteItem');
+    Route::get('deletecart', 'ProductController@deleteCart')->name('deleteCart');
+    Route::get('/add-to-wishlist/{id}','ProductController@getAddToWishlist')->name('addToWishlist');
 });
 
 
