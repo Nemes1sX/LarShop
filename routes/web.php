667376add9944f640400ce23c98913f009511bd8
Nemes1sX <kazstonk@gmail.com>
2025-07-25 23:06:44 +0300

    Add ui

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,20 @@
 <?php
 
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\StripeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::name('product.')->controller(\App\Http\Controllers\ProductController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::post('search', 'postSearch')->name('shop.search');
-    Route::post('sorting', 'ProductController@sorting')->name('shop.index');
-    Route::get('wishlist', 'ProductController@wishlist')->name('shop.wishlist');
-    Route::get('/add-to-wishlist/{id}','ProductController@getAddToWishlist')->name('addToWishlist');
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::prefix('/orders')->name('order.')->controller(OrderController::class)->group(function () {
-    Route::post('/store', 'store')->name('store');
-    Route::get('/test', 'test')->name('test');
-    Route::get('/success','callbackSuccess')->name('callback.success');
-    Route::get('/failed','callbackFailed')->name('callback.failed');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::post('/stripe/webhook', StripeController::class);
-Route::get('/cart',  [CartController::class, 'index'])->name('cart');
-Route::get('/home', 'HomeController@home')->name('home');
 
-
-
-
+require __DIR__.'/auth.php';
